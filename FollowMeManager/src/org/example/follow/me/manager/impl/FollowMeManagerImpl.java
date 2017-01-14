6b71dba231ac8f8d9cc4484cf4f39b4aa2999e91
Aymeric Auriol <ay.auriol@gmail.com>
2017-01-14 22:25:45 +0100

    exo 3 WIP

--- FollowMeManager/src/org/example/follow/me/manager/impl/FollowMeManagerImpl.java
+++ FollowMeManager/src/org/example/follow/me/manager/impl/FollowMeManagerImpl.java
@@ -1,5 +1,21 @@
 package org.example.follow.me.manager.impl;
 
-public class FollowMeManagerImpl {
+import org.example.follow.me.manager.FollowMeAdministration;
+import org.example.follow.me.manager.IlluminanceGoal;
+
+public class FollowMeManagerImpl implements FollowMeAdministration{
+
+	private IlluminanceGoal goal;
+	
+	@Override
+	public void setIlluminancePreference(IlluminanceGoal illuminanceGoal) {
+		goal = illuminanceGoal;
+		
+	}
+
+	@Override
+	public IlluminanceGoal getIlluminancePreference() {
+		return goal;
+	}
 
 }
